5dc4514aab5d35ba5bc71d3f086077e1444b11bc
Joh Yoshida <johyoshida@gmail.com>
2019-06-16 15:02:42 -0700

    App scaffolding

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Controls from "./components/Controls";
+import GameBoard from "./components/GameBoard";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: 25,
+      y: 25,
+      data: {}
+    };
+  }
+
+  render() {
+    const { x, y, data } = this.state;
+    return (
+      <div className="App">
+        <Controls
+          x={x}
+          y={y}
+        />
+        <GameBoard x={x} y={y} data={data} />
+      </div>
+    );
+  }
 }
 
 export default App;
